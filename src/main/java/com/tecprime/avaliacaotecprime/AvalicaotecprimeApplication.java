81c0535b49ad743962c42353b342481a7645e0fd
EvaldoRibeiro <65911994+EvaldoRibeiro@users.noreply.github.com>
2020-08-30 14:28:59 -0300

    Criando operacao de instanciacao

--- src/main/java/com/tecprime/avaliacaotecprime/AvalicaotecprimeApplication.java
+++ src/main/java/com/tecprime/avaliacaotecprime/AvalicaotecprimeApplication.java
@@ -1,13 +1,33 @@
 package com.tecprime.avaliacaotecprime;
 
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
-public class AvalicaotecprimeApplication {
+import com.tecprime.avaliacaotecprime.domain.Categoria;
+import com.tecprime.avaliacaotecprime.repositories.CategoriaRepository;
 
+@SpringBootApplication
+public class AvalicaotecprimeApplication implements CommandLineRunner{
+
+	@Autowired
+	private CategoriaRepository categoriaRepository;
+	
 	public static void main(String[] args) {
 		SpringApplication.run(AvalicaotecprimeApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		
+		Categoria cat1 = new Categoria(null, "Bebidas");
+		Categoria cat2 = new Categoria(null, "Alimentação");
+		
+		categoriaRepository.saveAll(Arrays.asList(cat1, cat2));
+		
 	}
 
 }
