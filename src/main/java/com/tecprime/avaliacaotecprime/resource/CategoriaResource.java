2f9f14c1e831a13b82340bc12352fe594f3d067c
EvaldoRibeiro <65911994+EvaldoRibeiro@users.noreply.github.com>
2020-08-31 09:46:11 -0300

    Atualizando Categoria com o PUT, outros pequenos ajustes

--- src/main/java/com/tecprime/avaliacaotecprime/resource/CategoriaResource.java
+++ src/main/java/com/tecprime/avaliacaotecprime/resource/CategoriaResource.java
@@ -36,14 +36,13 @@ public class CategoriaResource {
 		return ResponseEntity.created(uri).build();
 	}
 	
-/*	@RequestMapping(value="/{id}", method = RequestMethod.PUT)
-	public ResponseEntity<Void> update(@Valid @RequestBody CategoriaDTO objDto, @PathVariable Integer id){
-		Categoria obj = service.fromDTO(objDto);
+	@RequestMapping(value="/{id}", method = RequestMethod.PUT)
+	public ResponseEntity<Void> update(@RequestBody Categoria obj, @PathVariable Integer id){
 		obj.setId(id);
 		obj = service.update(obj);
 		return ResponseEntity.noContent().build(); 
 	}
-	@RequestMapping(value="/{id}", method=RequestMethod.DELETE)
+	/*	@RequestMapping(value="/{id}", method=RequestMethod.DELETE)
 	public ResponseEntity<Void> delete(@PathVariable Integer id) {
 		service.delete(id);
 		return ResponseEntity.noContent().build();
